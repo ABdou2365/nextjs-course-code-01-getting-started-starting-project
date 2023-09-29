a0142130e9b4469f427ddef58103b0ae4180d730
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2023-09-29 22:00:04 +0100

    using dynamics links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,35 @@
+import Link from "next/link";
+
 function ClientPage() { 
+
+    const clients = [
+        { 
+            id : "max",
+            name : "Maxumiliam"
+        },
+        {
+            id: "manu",
+            name : "Manuel"
+        },
+        {
+            id: "reda",
+            name : "Reda Kriate"
+        },
+        {
+            id: "abdou",
+            name :  "Abdellah Moutawakkil"
+        }
+    ]
     return ( 
         <div>
             <h1>The clients page</h1>
+            <ul>
+            {clients.map(client =>  <li key={client.id}>
+                    <Link href={`/clients/${client.id}`}>{client.name }</Link>
+                    </li>
+                )}
+            </ul>
+            
         </div>
     )
 }
