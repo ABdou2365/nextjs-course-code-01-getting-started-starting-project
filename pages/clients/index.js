639e362316cfb1e909e605470095b57ab307434b
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2023-09-29 22:11:36 +0100

    trying another way to set dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -25,7 +25,10 @@ function ClientPage() {
             <h1>The clients page</h1>
             <ul>
             {clients.map(client =>  <li key={client.id}>
-                    <Link href={`/clients/${client.id}`}>{client.name }</Link>
+                <Link href={{
+                    pathname: "/clients/[id]",
+                    query : { id : client.id }
+                    }}>{client.name }</Link>
                     </li>
                 )}
             </ul>
