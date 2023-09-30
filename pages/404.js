4f91a13688f4c9f9b7c19c17bbb057a18dc30764
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2023-09-30 18:55:44 +0100

    adding the 404 page

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+    return (
+        <div>
+            <h1>Oupss! Page not found</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage;
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,11 +2,18 @@ import { useRouter } from "next/router";
 
 function ClientProjectsPage() {
     const router = useRouter();
+
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[clientproduct]',
+        query : {id : "max" , clientproduct : "project A"}});
+    }
     console.log(router.query);
     return (
         
         <div>
             <h1>The client projects page</h1>
+            <button onClick={loadProjectHandler}>Load project A</button>
         </div>
     )
 }
